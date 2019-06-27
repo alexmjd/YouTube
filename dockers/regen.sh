29fd23a226666866e66b65a1dc66d4612739b4d4
Psyqozz <dylan.juzaine@gmail.com>
2019-06-27 20:27:07 +0200

    es

--- dockers/regen.sh
+++ dockers/regen.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker stop t_python t_encoder t_node t_mailer t_rabbit #t_db
-docker rm t_python t_encoder t_node t_mailer t_rabbit #t_db
+docker stop t_python t_encoder t_node t_mailer t_rabbit t_elastic #t_db
+docker rm t_python t_encoder t_node t_mailer t_rabbit t_elastic #t_db
 #docker rmi encodocker
 #docker rmi nodocker
 #docker rmi mailocker
 #docker rmi rabbitocker
+#docker rmi elasticker
 docker-compose up -d
