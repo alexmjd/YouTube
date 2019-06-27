4a36f05e3069628ff9e8a40169fd29505b55834e
alexandre <majda_a@etna-alternance.net>
2019-06-27 17:59:46 +0200

    add dockerfile_es and add service in docker-compose

--- dockers/regen.sh
+++ dockers/regen.sh
@@ -4,6 +4,6 @@ docker stop t_python t_encoder t_node t_mailer t_rabbit #t_db
 docker rm t_python t_encoder t_node t_mailer t_rabbit #t_db
 #docker rmi encodocker
 #docker rmi nodocker
-docker rmi mailocker
+#docker rmi mailocker
 #docker rmi rabbitocker
 docker-compose up -d
